112c8ebbda9e269f12815d2ea4335d765fd2871b
impressiveHen <b02505002@gmail.com>
2021-01-29 19:18:33 -0800

    change cookie name
    Signed-off-by: impressiveHen <b02505002@gmail.com>

--- src/main/java/com/beaconfireboba/authserver/constant/Constant.java
+++ src/main/java/com/beaconfireboba/authserver/constant/Constant.java
@@ -1,7 +1,7 @@
 package com.beaconfireboba.authserver.constant;
 
 public interface Constant {
-    String JWT_TOKEN_COOKIE_NAME = "JWT-TOKEN";
+    String JWT_TOKEN_COOKIE_NAME = "TEAMBOBA-JWT-TOKEN";
     String SIGNING_KEY = "teamboba-signingKey";
     int JWT_LOGIN_EXPIRE_MINUTES = 5;
 }
